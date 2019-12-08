e3fc3dabe81a353acf3afebcaa595c10536002d7
Elva Wu <wel20166@163.com>
2019-12-08 20:39:05 +0800

    whitening

--- app/src/main/java/com/example/face_beautification/WhiteningTranslater.java
+++ app/src/main/java/com/example/face_beautification/WhiteningTranslater.java
@@ -8,7 +8,7 @@ import org.opencv.core.Mat;
 public class WhiteningTranslater extends Translater {
 
     public WhiteningTranslater() {
-        super("Whitening", "美白", false, 0);
+        super("Whitening", "美白", false, 6);
     }
 
     @Override
@@ -18,9 +18,11 @@ public class WhiteningTranslater extends Translater {
         for (int row = 0; row < mat.rows(); row++) {
             for (int col = 0; col < mat.cols(); col++) {
                 double[] rgb = mat.get(row, col);
-                rgb[0] += level;
-                rgb[1] += level;
-                rgb[2] += level;
+                double preBrightness =rgb[0]*0.299+rgb[1]*0.587+rgb[2]*0.114;
+                double brightnessIncreRate = (Math.log(preBrightness*(level-1)+1)/Math.log(level))/preBrightness;
+                rgb[0]=rgb[0]*brightnessIncreRate;
+                rgb[1]=rgb[1]*brightnessIncreRate;
+                rgb[2]=rgb[2]*brightnessIncreRate;
                 mat.put(row, col, rgb);
             }
         }
