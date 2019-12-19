ad24aa64b6b4140add27a51a6c6f7c086f027be1
Elva Wu <wel20166@163.com>
2019-12-20 00:58:22 +0800

    美白用Bitmap

--- app/src/main/java/com/example/face_beautification/WhiteningTranslater.java
+++ app/src/main/java/com/example/face_beautification/WhiteningTranslater.java
@@ -1,6 +1,7 @@
 package com.example.face_beautification;
 
 import android.graphics.Bitmap;
+import android.graphics.Color;
 
 import org.opencv.android.Utils;
 import org.opencv.core.CvType;
@@ -21,10 +22,10 @@ public class WhiteningTranslater extends Translater {
     @Override
     Bitmap render(PictureManager pictureManager, Bitmap bitmap) {
         if (level > 0) {
-            Mat mat = new Mat();
-            Utils.bitmapToMat(bitmap, mat);
+            //Mat mat = new Mat();
+            //Utils.bitmapToMat(bitmap, mat);
             //去掉透明通道
-            Imgproc.cvtColor(mat, mat, Imgproc.COLOR_RGBA2BGR);
+            //Imgproc.cvtColor(mat, mat, Imgproc.COLOR_RGBA2BGR);
             //如果用到了这个效果再做
             if (level > 0) {
                 //Imgproc.blur(mat, mat, new Size(level, level));
@@ -35,28 +36,34 @@ public class WhiteningTranslater extends Translater {
 //            System.out.println(mat.type());
 //            System.out.println(CvType.typeToString(mat.type()));
 //            Mat test = new Mat(mat.size(), CvType.CV_32FC3, new Scalar(0.01 * level / 100, 0.01 * level, 0.01 * level));
-                mat.convertTo(mat, CvType.CV_32FC3, 1.0 / 255);
+               // mat.convertTo(mat, CvType.CV_32FC3, 1.0 / 255);
                 //Imgproc.accumulate(test, mat);
 
-                double[] pixel = new double[3];
-                for (int i = 0, rlen = mat.rows(); i < rlen; i++) {
-                    for (int j = 0, clen = mat.cols(); j < clen; j++) {
-                        pixel = mat.get(i, j).clone();
-                        double brightness = 0.299 * pixel[0] + 0.587 * pixel[1] + 0.114 * pixel[2];
-                        double brightnessIncre = (Math.log(brightness * (0.2 * level - 1) + 1) / Math.log(0.2 * level)) - brightness;//level限制在0-20
-                        pixel[0] = pixel[0] + brightnessIncre / (0.299 + 0.587 + 0.114);//R
-                        pixel[1] = pixel[1] + brightnessIncre / (0.299 + 0.587 + 0.114);//G
-                        pixel[2] = pixel[2] + brightnessIncre / (0.299 + 0.587 + 0.114);//B
-                        mat.put(i, j, pixel);
+                int clen=bitmap.getWidth(), rlen=bitmap.getHeight();
+                int pixel;
+                int[] newPixels = new int[rlen * clen];
+
+                for (int i = 0; i < rlen; i++) {
+                    for (int j = 0; j < clen; j++) {
+                        pixel = bitmap.getPixel(i,j);
+                        int r= Color.red(pixel);
+                        int g= Color.green(pixel);
+                        int b= Color.blue(pixel);
+                        double brightness=0.299*r+0.587 * g+0.114 * b;
+                        double brightnessIncre=Math.log ( brightness *(level-1)+1 ) / Math.log(level)  - brightness ;
+                        r = r+(int)brightnessIncre>255 ? 255: r+(int)brightnessIncre;//R
+                        g = g+(int)brightnessIncre>255 ? 255: g+(int)brightnessIncre;//G
+                        b = b+(int)brightnessIncre>255 ? 255: b+(int)brightnessIncre;//B
+                        newPixels[i * rlen + j] = Color.rgb(r,g,b);
 
                     }
                 }
-
-                mat.convertTo(mat, CvType.CV_8UC3, 255);
+                bitmap.setPixels(newPixels, 0, clen,0,0,clen,rlen);
+                //mat.convertTo(mat, CvType.CV_8UC3, 255);
 
             }
-            Imgproc.cvtColor(mat, mat, Imgproc.COLOR_BGR2RGBA);
-            Utils.matToBitmap(mat, bitmap);
+            //Imgproc.cvtColor(mat, mat, Imgproc.COLOR_BGR2RGBA);
+            //Utils.matToBitmap(mat, bitmap);
             return bitmap;
         } else {
             return bitmap;
